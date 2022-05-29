56cec46afe5054267ef48458f2d1e1300594a481
y4suyuki <y4suyuki@pm.me>
2022-05-30 00:41:40 +0900

    load scripts defined in manifest

--- src/background.ts
+++ src/background.ts
@@ -77,22 +77,3 @@ chrome.runtime.onMessage.addListener(async function (
       console.log("%cNothing matched", "color: green;");
   }
 });
-
-chrome.tabs.onCreated.addListener((tab) => {
-  console.log("%ctab is created!", "color: green;");
-  console.log(tab);
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["dist/content-script.js"],
-  });
-});
-
-chrome.tabs.onUpdated.addListener((tabId) => {
-  console.log(`%ctab[${tabId}] is updated!`, "color: green;");
-
-  chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    files: ["dist/content-script.js"],
-  });
-});
